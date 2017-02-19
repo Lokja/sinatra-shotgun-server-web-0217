375a7b745f6dc366559d73af2225ce25f2485d0b
Devin Rieger <dev0089@gmail.com>
2017-02-19 13:53:54 -0500

    finished readme/walkthrough

--- app.rb
+++ app.rb
@@ -2,8 +2,8 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
-  get '/' do 
-    "Welcome to your app!!!! "
+  get '/' do
+    "Welcome to your app WOOOOO!!!! SHOTTIES SNPERS BLOOD GULCH NO VEHICLES "
   end
 
 end
